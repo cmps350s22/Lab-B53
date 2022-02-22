ad29a3dbb97aab12008b2ce54ed591694621cc9f
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-02-22 15:36:42 +0300

    updated part 1

--- Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab 7 - Client JS/Tutorial/Country Explorer/js/index.js
@@ -1,7 +1,28 @@
 const regionSearchBar = document.querySelector('#region')
 const countryDropdown = document.querySelector('#country')
 
-function getCountries() {
-    const selectedCountry = regionSearchBar.value
-    alert(selectedCountry)
+//APIS Endpoints
+const regionBaseUrl = 'https://restcountries.com/v3.1/region'
+
+
+async function getCountries() {
+    const selectedRegion = regionSearchBar.value
+    const url = `${regionBaseUrl}/${selectedRegion}`
+    const data = await fetch(url) //suspend
+    //parse the data
+    const countries = await data.json()
+    const options = countries.map(country =>  `<option value="${country.name.common}">${country.name.common}</option>`)
+    countryDropdown.innerHTML = options.join(' ')
 }
+
+
+
+
+
+
+
+
+
+
+
+
