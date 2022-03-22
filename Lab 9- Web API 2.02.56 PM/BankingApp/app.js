7234e2200b80ffd3b16f1a3e680b0b34a60b67d5
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-22 15:23:59 +0300

    Update app.js

--- Lab 9- Web API 2.02.56 PM/BankingApp/app.js
+++ Lab 9- Web API 2.02.56 PM/BankingApp/app.js
@@ -3,20 +3,19 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000
 
-//http://localhost:3000/api/accounts
-//http://localhost:3000/api/accounts?type=current
-
-app.get('/api/accounts/:acctNo',(req, res)=>{
-    res.send(req.params)
-})
-
 app.get('/api/accounts', (req, res) => {
     const query = req.query
     // res.send('this route will return all the accounts')
-    res.send(req.query)
+    res.send(`getting accounts of type ${req.query.type}`)
 })
 
+app.get('/api/accounts/:acctNo',(req, res)=>{
+    res.send(`getting single account with account no ${req.params.acctNo}`)
+})
 
+app.delete('/api/accounts/:acctNo',(req, res)=>{
+    res.send(`deleting ${req.params.acctNo}`)
+})
 
 app.listen(port, () => {
     console.log(`server started on http://localhost:${port}`)
@@ -31,5 +30,9 @@ app.listen(port, () => {
 
     3.download express framework
     npm i express
+
+    4. install nodemon [only once in the entire semester]
+    npm i -g nodemon    [window user]
+    sudo npm i -g nodemon [mac users]
 
  */
