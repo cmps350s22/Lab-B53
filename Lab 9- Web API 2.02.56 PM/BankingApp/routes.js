86c2dda61f7edf883efdc1c4d2ab6d00762ce9f6
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-03-22 16:54:54 +0300

    updated

--- Lab 9- Web API 2.02.56 PM/BankingApp/routes.js
+++ Lab 9- Web API 2.02.56 PM/BankingApp/routes.js
@@ -1,53 +1,19 @@
 
 //install a json middleware
 import express from "express";
+import BankService from './service/bank-service.js'
+const bankService = new BankService();
 const router = express.Router();
-const accounts = [
-    {
-        accountNo: 1101,
-        acctType: "Current",
-        balance: 500
-    },
-    {
-        accountNo: 1155,
-        acctType: "Current",
-        balance: 500
-    },
-    {
-        accountNo: 1102,
-        acctType: "Saving",
-        balance: 500
-    }
-]
 
 router.use(express.json())
-//http://localhost.:3000/api/accounts?type=Saving
-router.get('/accounts', (req, res) => {
-    const acctType = req.query.type
-    if (acctType) {
-        const filteredAccounts = accounts.filter(account => account.acctType == acctType)
-        res.json(filteredAccounts)
-    }
-    else
-        res.json(accounts)
-})
+router.route('/accounts')
+    .get(bankService.getAccounts)
+    .put(bankService.updateAccount)
+    .post(bankService.addAccount)
 
-router.post('/accounts', (req, res) => {
-    // res.send('this route will add an account')
-    res.json(req.body)
-})
+router.route('/accounts/:acctNo')
+    .get(bankService.getAccount)
+    .delete(bankService.deleteAccount)
 
-router.put('/accounts', (req, res) => {
-    // res.send('this route will update accounts')
-    res.json(req.body)
-})
-
-router.get('/accounts/:acctNo', (req, res) => {
-    res.send(`getting single account with account no ${req.params.acctNo}`)
-})
-
-router.delete('/accounts/:acctNo', (req, res) => {
-    res.send(`deleting ${req.params.acctNo}`)
-})
 
 export default router;
