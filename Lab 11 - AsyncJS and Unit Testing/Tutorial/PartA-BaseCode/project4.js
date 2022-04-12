29519a1eb92435a64b63a0f337a2303980d18569
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-12 20:07:28 +0300

    updated

--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project4.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project4.js
@@ -1,7 +1,31 @@
-
-const fs=require('fs-extra');
+import fs from 'fs-extra'
 
 
 /*
 1.Rewrite the you created in Part-2 using promises.
 */
+
+fs.readJson('data/course.json')
+    .then(courses => {
+         return fs.readJson('data/staff.json')
+            .then(staffs => {
+                for (const course of courses) {
+                    const {firstname, lastname} = staffs.find(staff => staff.staffNo == course.instructorId)
+                    course.instructor = `${firstname} ${lastname}`
+                }
+                return courses
+            })
+    })
+    .then(courses => {
+        return fs.readJson('data/student.json')
+            .then(students => {
+                for (const course of courses) {
+                    //crn and courseIds
+                    course.students = students
+                        .filter(student => student.courseIds.includes(course.crn))
+                        .map(student => `${student.firstname} ${student.lastname}`)
+                }
+                return courses
+            })
+    })
+    .then(courses => console.log(courses))
--- Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project7.js
+++ Lab 11 - AsyncJS and Unit Testing/Tutorial/PartA-BaseCode/project7.js
@@ -1,4 +1,23 @@
-const fs = require('fs-extra');
+import fs from 'fs-extra'
 
 /* 1.	In project7.js :  Rewrite the code you created in project5.js using Async/await.
 */
+
+async function getCourses() {
+    const courses = await fs.readJson('data/course.json')
+    const staffs = await fs.readJson('data/staff.json')
+    const students = await fs.readJson('data/student.json')
+
+    for (const course of courses) {
+        const {firstname, lastname} = staffs.find(staff => staff.staffNo == course.instructorId)
+        course.instructor = `${firstname} ${lastname}`
+        course.students = students
+            .filter(student => student.courseIds.includes(course.crn))
+            .map(student => `${student.firstname} ${student.lastname}`)
+    }
+    return courses
+}
+
+getCourses().then(courses=> console.log(courses))
+
+console.log('this will not be blocked')
