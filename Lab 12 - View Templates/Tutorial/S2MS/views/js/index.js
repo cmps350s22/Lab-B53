54301221e4b961229ae87e7cac1919a78c377d15
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-19 20:03:54 +0300

    final

---
+++ Lab 12 - View Templates/Tutorial/S2MS/views/js/index.js
@@ -0,0 +1,29 @@
+const template = `
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Program</th>
+                <th>Code</th>
+                <th>Name</th>
+           </tr>
+         </thead>
+        <tbody>
+        {{#each courses as |c|}}
+            <tr>
+                <td>{{c.program}}</td>
+                <td>{{c.code}}</td>
+                <td>{{c.name}}</td>
+            </tr>
+        {{/each}}
+        </tbody>
+    </table>
+`
+
+async function getCourses(programCode) {
+    // http://localhost:5000/api/courses/CE
+    const data = await fetch(`/api/courses/${programCode}`)
+    const courses = await data.json()
+    const compiledTemplate = Handlebars.compile(template)
+    const htmlTableCode = compiledTemplate({courses})
+    document.querySelector('#courses-table').innerHTML = htmlTableCode
+}
