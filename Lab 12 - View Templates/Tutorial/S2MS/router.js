a73f4d266f319ec9b6e50a4fd5a44d35cf59f53d
Abdulahi Hassen <abdala.baaji@gmail.com>
2022-04-19 15:04:29 +0300

    lab 12-updated hbs

---
+++ Lab 12 - View Templates/Tutorial/S2MS/router.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import CourseService from './service/course-service.js'
+const courseService = new CourseService()
+
+const router = express.Router()
+//Render
+router.get('/',courseService.renderIndex)
+router.get('/courses',courseService.renderCourses)
+
+//API end points
+router.get('/api/programs',courseService.getPrograms);
+router.get('/api/courses/:programCode',courseService.getCourses);
+
+export default router
